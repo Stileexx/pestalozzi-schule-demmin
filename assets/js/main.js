7befe475e7f71bb9658dbb7567f68f93072a3b4b
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-16 16:43:30 +0200

    navbar for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,33 @@
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
+// Funktion zum Zeigen des Hauptmenüs
+const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+        nav = document.getElementById(navId);
 
-  toggle.addEventListener('click', () =>{
-      // Add show-menu class to nav menu
-      nav.classList.toggle('show-menu')
+  // Hauptmenü-Toggle Event Listener
+  if (toggle) {
+      toggle.addEventListener('click', () => {
+          nav.classList.toggle('show-menu');
+      });
+  }
+};
 
-      // Add show-icon to show and hide the menu icon
-      toggle.classList.toggle('show-icon')
-  })
-}
+// Funktion zum Toggeln des Submenüs
+const setupSubmenuToggles = () => {
+  const nav = document.getElementById('nav-menu');
+  const dropdownLinks = nav.querySelectorAll('.dropdown__link');
+  
+  dropdownLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+          const parentItem = link.parentElement;
+          const submenu = parentItem.querySelector('.dropdown__submenu');
+          if (submenu) {
+              submenu.classList.toggle('submenu-show');
+              event.preventDefault(); // Verhindern des Standard-Linkverhaltens
+          }
+      });
+  });
+};
 
-showMenu('nav-toggle','nav-menu')
+// Funktionen initialisieren
+showMenu('nav-toggle', 'nav-menu');
+setupSubmenuToggles();
